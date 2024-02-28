665b1b5e0ca12d1f95e6dbb5e1961b0eee963b9d
w3cj <cj@null.computer>
2024-02-28 09:03:57 -0700

    update command

--- config.sh
+++ config.sh
@@ -2,9 +2,7 @@
 
 DOTFILES=(.bash_profile .gitconfig .gitignore .zshrc)
 
-#Remove old dotfiles and replace them
 for dotfile in $(echo ${DOTFILES[*]});
 do
-    rm ~/$(echo $dotfile)
-    ln -s ~/dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
+    cp ~/dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
 done
